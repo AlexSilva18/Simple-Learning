902a293f2b4529adc76af56570036099d07b73e1
genius <alex.rsilva18@gmail.com>
2017-10-17 10:18:16 -0400

    first commit

---
+++ learn.c
@@ -0,0 +1,12 @@
+#include "learn.h"
+
+int main(int argc, char *argvp[])
+{
+  if (argc != 3)
+    {
+      fprintf(stderr, "Incorrect Input\n");
+    }
+
+  
+  
+}
---
+++ learn.h
@@ -0,0 +1,36 @@
+#ifndef LEARN_H
+#define LEARN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Predict the price of the house for each entry in the test data file*/
+
+/*GET TRAINING DATA FILE
+
+  1st line has int K = # of attributes
+  2nd line has int N = # of training exampels in the training data set
+  Nth lines has the N training examples.(list of comma seperated double precision floating point values.)
+  - the first K double precision values represent the values for the attributes of the house
+  - the last double precision value in the line represents the price of the house1
+
+  to do:
+       - read 1st line line to know the number of attributes to be stored in the Nth lines as attributes
+       - last line will be stored as the price
+       - read 2nd line to know how many training examples there are going to be to loop that many times through the file
+*/
+
+/*GET TESTING DATA FILES
+  
+  1st line has integer M = # of test data points in the file
+
+*/
+
+//read the training data
+//transpose a matrix
+//inverse a matrix
+//multiply matrices
+
+
+#endif
