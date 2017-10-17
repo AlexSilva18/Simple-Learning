d7b030e493cb49746464d4409db17515d0b6e824
Alex <alex.rsilva18@gmail.com>
2017-10-17 13:13:38 -0400

    get input

--- learn.c
+++ learn.c
@@ -1,12 +1,14 @@
 #include "learn.h"
 
-int main(int argc, char *argvp[])
+int main(int argc, char *argv[])
 {
   if (argc != 3)
     {
       fprintf(stderr, "Incorrect Input\n");
     }
+  
+  getTrainingData(argv[1]);
+  //getTestData(argv[2]);
 
-  
-  
+  return 0;
 }
--- learn.h
+++ learn.h
@@ -5,6 +5,55 @@
 #include <stdlib.h>
 #include <string.h>
 
+void getTrainingData(char *file);
+void getTestData(char *file);
+
+void getTrainingData(char *file)
+{
+  FILE *fp = fopen(file, "r");
+  int N, K = 0;
+  int i,j;
+  
+  if (fp == NULL)
+    {
+      printf("0\n");
+    }
+
+  fscanf(fp, "%d\n", &K);
+  fscanf(fp, "%d\n", &N);
+  //printf("K %d, N is %d", K, N);
+
+  float data_matrix[N][K-1];
+
+  for (i=0; i<N; i++)
+    {
+      for (j=0;j<(K-1); j++)
+	{
+	  fscanf(fp,"%f", &data_matrix[i][j]);
+	  if (fscanf(fp, "^,", 
+	}
+      fscanf(fp,"\n");
+    }
+
+  
+  for (i=0; i<N; i++)
+    {
+      for (j=0;j<(K-1); j++)
+	{
+	  printf("%f\t", data_matrix[i][j]);
+	  if (j== K-2)
+	    {
+	      printf("\n");
+	    }
+		 
+	}
+    }
+  
+  
+}
+
+
+
 /*Predict the price of the house for each entry in the test data file*/
 
 /*GET TRAINING DATA FILE
@@ -16,21 +65,25 @@
   - the last double precision value in the line represents the price of the house1
 
   to do:
-       - read 1st line line to know the number of attributes to be stored in the Nth lines as attributes
-       - last line will be stored as the price
-       - read 2nd line to know how many training examples there are going to be to loop that many times through the file
+       - read 1st line to know the number of attributes to be stored in the Nth lines as attributes (columns)
+       - read 2nd line to know how many training examples to be stored as K (rows)
+       - last line will be stored as the price in a new matrix (N x 1)
+       - The matrix size is going to be equal to N x K
 */
 
 /*GET TESTING DATA FILES
   
   1st line has integer M = # of test data points in the file
+  K is the same as getTrainingData
+  
 
 */
 
-//read the training data
-//transpose a matrix
-//inverse a matrix
-//multiply matrices
+// read the training data
+// read the test data
+// store them in matrices
+// transpose getTrainingData, multiply it by the regular matrix, inverse it, * by transpose, * getTestData
+// W = (X^T * X)^-1 * X^T * Y
 
 
 #endif
