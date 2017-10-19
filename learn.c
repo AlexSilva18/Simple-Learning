7e6b581c43d1794c711143e7485dc9d83ee16207
Alex <alex.rsilva18@gmail.com>
2017-10-19 00:34:09 -0400

    X and Y

--- learn.c
+++ learn.c
@@ -27,20 +27,20 @@ int main(int argc, char *argv[])
   double **X;
   double *Y;
 
-  X = (double**)malloc(sizeof(double*)*K+1);
-  Y = (double*)malloc(sizeof(double)*K);
+  X = (double**)malloc(sizeof(double*)*(N));
+  Y = (double*)malloc(sizeof(double)*N);
   
   
   //allocate matrix memory
-  for (i=0; i<K+1; i++)
+  for (i=0; i<N; i++)
     {
-      X[i] = (double*)malloc(sizeof(double)*N);
+      X[i] = (double*)malloc(sizeof(double)*(K+1));
     }
   
   getTrainingData(file, X, Y, N, K);
   //getTestData(argv[2]);
 
-  for (i=0; i<K+1; i++)
+  for (i=0; i<N; i++)
     {
       free(X[i]);
     }
@@ -56,64 +56,40 @@ void getTrainingData(FILE *fp, double **X, double *Y, int N, int K)
 {
   int i, j;
 
-  for (i = 0; i < N; i++)
+  //test case
+  /*for (i = 0; i < 11; ++i)
     {
-      X[i][0] = 1;
-    }
+      X[i][0] = 1.0;
+      printf("%lf\n", X[i][0]);
+    }*/
   
+  for (i=0; i<N; i++)
+    {
+      X[i][0] = 1.0;
+      for (j=1; j < (K+1); j++)
+	{
+	  fscanf(fp,"%lf,", &X[i][j]);
+	}
+      fscanf(fp,"%lf,", &Y[i]);
+    }
 
+  //Print matrix X
   for (i=0; i<N; i++)
     {
       for (j=0;j<K+1; j++)
 	{
 	  printf("%lf\t", X[i][j]);
-	  if (j== K)
+	  if (j == K)
 	    {
 	      printf("\n");
 	    }
-	  
 	}
     }
-    
   
-  
-      /*for (i=0; i<N; i++)
+  //Print matrix Y
+  for (i = 0; i<N; i++)
     {
-      X[i][0] = 1;
-      for (j=1; j<( K+1); j++)
-	{
-	  //fscanf(fp,"%lf,", &X[i][j]);
-	  //fscanf(fp,"%lf", &Y[i]);
-	}
-      
-	}*/
-
-  /*X[0][0] = 1;
-  X[1][0] = 1;
-  X[2][0] = 1;
-  X[3][0] = 1;
-  X[4][0] = 1;
-  X[5][0] = 1;
-  printf("%lf\t\n", X[0][0]);
-  printf("%lf\t\n", X[1][0]);
-  printf("%lf\t\n", X[2][0]);
-  printf("%lf\t\n", X[3][0]);
-  printf("%lf\t\n", X[4][0]);
-  printf("%lf\t\n", X[5][0]);*/
-  
-  
-  /*for (i=0; i<N; i++)
-    {
-      for (j=0;j<K+1; j++)
-	{
-	  printf("%lf\t", X[i][j]);
-	  if (j== K)
-	    {
-	      printf("\n");
-	    }
-	  
-	}
-    }*/
-  
-  
+      printf("%lf\t", Y[i]);
+    }
+   
 }
